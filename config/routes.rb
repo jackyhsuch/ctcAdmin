3374b8c9a1e012c107daccfac9efd728c3d9857b
jackyhsuch <jackyhsu0102@gmail.com>
2016-11-24 15:33:29 +0800

    add in bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   get 'admin_users/new'
 
   get 'users/new'
 
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'sessions#new'
+
+  resources :admin_users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
